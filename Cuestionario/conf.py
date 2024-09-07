3e01eb8173beeb4586c0d22428b960afe5acb5be
GaelFG17 <167834489+GaelFG17@users.noreply.github.com>
2024-09-06 20:34:32 -0600

    Update conf.py

--- Cuestionario/conf.py
+++ Cuestionario/conf.py
@@ -4,3 +4,6 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://username:password@localhost/db_name'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+#en lugar ussername va tu usuario de mysql,en lugar de password tu contraseña y al final en lugar de db_name el nombre de la bd a usar
